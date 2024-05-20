d415afb080d87a10ebc8ae106e4b1075f6dcd615
seahi <seahi-mo@foxmail.com>
2024-05-20 23:55:40 +0800

    添加部分文件夹

--- software/projectCode/components/bouffalo.mk
+++ software/projectCode/components/bouffalo.mk
@@ -1,6 +1,8 @@
 
-
-
 COMPONENT_ADD_INCLUDEDIRS := ../../Ai-M6x_HomeAssistant-C/HomeAssistant-C
-COMPONENT_OBJS := $(patsubst %.c,%.o, $(COMPONENT_SRCS))
+COMPONENT_ADD_INCLUDEDIRS += at_command device_state flash homeAssistant  ir_hxd019 wifi
+
 COMPONENT_SRCDIRS := ../../Ai-M6x_HomeAssistant-C/HomeAssistant-C
+COMPONENT_SRCDIRS += at_command device_state flash homeAssistant  ir_hxd019 wifi
+
+COMPONENT_OBJS := $(patsubst %.c,%.o, $(COMPONENT_SRCS))
---
+++ software/projectCode/components/device_state/device_state.h
@@ -0,0 +1,10 @@
+/**
+ * @file device_state.h
+ * @author Seahi (seahi-mo@foxmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-05-20
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+*/
